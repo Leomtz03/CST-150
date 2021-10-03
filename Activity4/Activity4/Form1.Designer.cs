10ccedbb3cb43db9fdfe5f83d167d9c9f7259b2d
Leomtz03 <79104199+Leomtz03@users.noreply.github.com>
2021-10-03 09:40:12 -0700

    First commit

---
+++ Activity4/Activity4/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace Activity4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.enterLabel = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.resetButton);
+            this.groupBox1.Controls.Add(this.submitButton);
+            this.groupBox1.Controls.Add(this.inputTextBox);
+            this.groupBox1.Controls.Add(this.enterLabel);
+            this.groupBox1.Location = new System.Drawing.Point(28, 27);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(7, 7, 7, 7);
+            this.groupBox1.Size = new System.Drawing.Size(873, 395);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Elapsed Time";
+            // 
+            // resetButton
+            // 
+            this.resetButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.resetButton.Location = new System.Drawing.Point(324, 219);
+            this.resetButton.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(175, 51);
+            this.resetButton.TabIndex = 3;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // submitButton
+            // 
+            this.submitButton.Location = new System.Drawing.Point(579, 219);
+            this.submitButton.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(175, 51);
+            this.submitButton.TabIndex = 2;
+            this.submitButton.Text = "Submit";
+            this.submitButton.UseVisualStyleBackColor = true;
+            this.submitButton.Click += new System.EventHandler(this.submitButton_Click);
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Location = new System.Drawing.Point(548, 87);
+            this.inputTextBox.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(228, 35);
+            this.inputTextBox.TabIndex = 1;
+            // 
+            // enterLabel
+            // 
+            this.enterLabel.AutoSize = true;
+            this.enterLabel.Location = new System.Drawing.Point(65, 94);
+            this.enterLabel.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.enterLabel.Name = "enterLabel";
+            this.enterLabel.Size = new System.Drawing.Size(419, 29);
+            this.enterLabel.TabIndex = 0;
+            this.enterLabel.Text = "Enter the number of seconds elapsed:";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.submitButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.resetButton;
+            this.ClientSize = new System.Drawing.Size(923, 441);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
+            this.Name = "Form1";
+            this.Text = "Activity 4";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.Label enterLabel;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.Button submitButton;
+    }
+}
+
---
+++ Activity4/Activity4/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void submitButton_Click(object sender, EventArgs e)
+        {
+            int secondsInput;
+            int seconds;
+            int minutes;
+            int hours;
+            int days;
+            int remainder;
+
+            if (int.TryParse(inputTextBox.Text, out secondsInput))
+            {
+                if (secondsInput >= 60 && secondsInput < 3600)
+                {
+                    minutes = secondsInput / 60;
+                    seconds = secondsInput % 60;
+                    MessageBox.Show(minutes + " minutes" + " " + seconds + " seconds");
+                }
+                else if (secondsInput >= 3600 && secondsInput < 86400)
+                {
+                    hours = secondsInput / 3600;
+                    remainder = secondsInput % 3600;
+                    minutes = remainder / 60;
+                    seconds = remainder % 60;
+                    MessageBox.Show(hours + " hours" + " " + minutes + " minutes" + " " + seconds + " seconds");
+                }
+                else if (secondsInput >= 86400)
+                {
+                    days = secondsInput / 86400;
+                    remainder = secondsInput % 86400;
+                    hours = remainder / 3600;
+                    remainder = remainder % 3600;
+                    minutes = remainder / 60;
+                    seconds = remainder % 60;
+                    MessageBox.Show(days + " days" + " " + hours + " hours" + " " + minutes +  " minutes" + " " + seconds + " seconds");
+                }
+                else
+                {
+                    MessageBox.Show("Enter an integer higher than 60 seconds.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Enter a valid integer.");
+            }
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            inputTextBox.Text = "";
+        }
+    }
+}
