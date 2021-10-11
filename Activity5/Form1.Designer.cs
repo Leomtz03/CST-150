b12c38873f26451797a3bc5b3cb3e975e6989ae1
Leomtz03 <79104199+Leomtz03@users.noreply.github.com>
2021-10-10 23:27:11 -0700

    First commit

---
+++ Activity5/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿
+namespace Activity5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.contentLabel = new System.Windows.Forms.Label();
+            this.detailedListBox = new System.Windows.Forms.ListBox();
+            this.processButton = new System.Windows.Forms.Button();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.openFile = new System.Windows.Forms.OpenFileDialog();
+            this.saveFile = new System.Windows.Forms.SaveFileDialog();
+            this.openButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.richTextBox = new System.Windows.Forms.RichTextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("groupBox1.BackgroundImage")));
+            this.groupBox1.Controls.Add(this.richTextBox);
+            this.groupBox1.Controls.Add(this.saveButton);
+            this.groupBox1.Controls.Add(this.openButton);
+            this.groupBox1.Controls.Add(this.resetButton);
+            this.groupBox1.Controls.Add(this.processButton);
+            this.groupBox1.Controls.Add(this.detailedListBox);
+            this.groupBox1.Controls.Add(this.contentLabel);
+            this.groupBox1.ForeColor = System.Drawing.Color.White;
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(425, 470);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Activity 5";
+            // 
+            // contentLabel
+            // 
+            this.contentLabel.AutoSize = true;
+            this.contentLabel.BackColor = System.Drawing.Color.Transparent;
+            this.contentLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.contentLabel.Location = new System.Drawing.Point(52, 60);
+            this.contentLabel.Name = "contentLabel";
+            this.contentLabel.Size = new System.Drawing.Size(61, 17);
+            this.contentLabel.TabIndex = 0;
+            this.contentLabel.Text = "Content:";
+            // 
+            // detailedListBox
+            // 
+            this.detailedListBox.BackColor = System.Drawing.Color.White;
+            this.detailedListBox.FormattingEnabled = true;
+            this.detailedListBox.Location = new System.Drawing.Point(55, 295);
+            this.detailedListBox.Name = "detailedListBox";
+            this.detailedListBox.Size = new System.Drawing.Size(308, 95);
+            this.detailedListBox.TabIndex = 2;
+            // 
+            // processButton
+            // 
+            this.processButton.BackColor = System.Drawing.Color.MidnightBlue;
+            this.processButton.Location = new System.Drawing.Point(288, 424);
+            this.processButton.Name = "processButton";
+            this.processButton.Size = new System.Drawing.Size(75, 23);
+            this.processButton.TabIndex = 3;
+            this.processButton.Text = "Process";
+            this.processButton.UseVisualStyleBackColor = false;
+            this.processButton.Click += new System.EventHandler(this.processButton_Click);
+            // 
+            // resetButton
+            // 
+            this.resetButton.BackColor = System.Drawing.Color.MidnightBlue;
+            this.resetButton.Location = new System.Drawing.Point(205, 424);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(75, 23);
+            this.resetButton.TabIndex = 4;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = false;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // openFile
+            // 
+            this.openFile.FileName = "openFileDialog1";
+            // 
+            // openButton
+            // 
+            this.openButton.BackColor = System.Drawing.Color.MidnightBlue;
+            this.openButton.Location = new System.Drawing.Point(205, 33);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(75, 23);
+            this.openButton.TabIndex = 5;
+            this.openButton.Text = "Open";
+            this.openButton.UseVisualStyleBackColor = false;
+            this.openButton.Click += new System.EventHandler(this.openButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.BackColor = System.Drawing.Color.MidnightBlue;
+            this.saveButton.Location = new System.Drawing.Point(288, 33);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 6;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = false;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // richTextBox
+            // 
+            this.richTextBox.BackColor = System.Drawing.Color.White;
+            this.richTextBox.Location = new System.Drawing.Point(55, 85);
+            this.richTextBox.Name = "richTextBox";
+            this.richTextBox.Size = new System.Drawing.Size(308, 195);
+            this.richTextBox.TabIndex = 7;
+            this.richTextBox.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(447, 496);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Activity 5";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button openButton;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.Button processButton;
+        private System.Windows.Forms.ListBox detailedListBox;
+        private System.Windows.Forms.Label contentLabel;
+        private System.Windows.Forms.OpenFileDialog openFile;
+        private System.Windows.Forms.SaveFileDialog saveFile;
+        private System.Windows.Forms.RichTextBox richTextBox;
+    }
+}
+
---
+++ Activity5/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Activity5
+{
+    public partial class Form1 : Form
+    {
+        List<String> words = new List<string>();
+        BindingSource bs = new BindingSource();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openButton_Click(object sender, EventArgs e)
+        {
+            //OpenFileDialog openFile = new OpenFileDialog();
+            
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    // Declare a StreamWriter variable.
+                    StreamReader inputFile;
+                    
+                    // Open file from Debug folder for now.
+                    inputFile = File.OpenText("test.txt");
+
+                    // Read text from file.
+                    string text = inputFile.ReadToEnd();
+
+                    // Assign all text to be displayed in richTextBox control.
+                    richTextBox.Text = text;
+                
+                    // Close file.
+                    inputFile.Close();
+                }
+                catch (Exception ex)
+                {
+                    // Display an error message.
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
+                // The operation was canceled.
+            }
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            
+
+            if (saveFile.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    // Declare a StreamWriter variable.
+                    StreamWriter outputFile;
+
+                    // Allow the user to create and name a file and get a StreamWriter object.
+                    outputFile = File.CreateText(saveFile.FileName);
+                    
+                    // Write the text from richTextBox to the file.
+                    outputFile.WriteLine(richTextBox.Text);
+
+                    // Close the file.
+                    outputFile.Close();
+                }
+                catch (Exception ex)
+                {
+                    // Display an error Message.
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
+                // The operation was canceled.
+            }
+        }
+
+        private void processButton_Click(object sender, EventArgs e)
+        {
+            // Assign richTextBox text a variable.
+            string text = richTextBox.Text;
+
+            // Convert all text to lowercase.
+            string lowerText = text.ToLower();
+
+            // Assign lowercase text back to richTextBox.
+            richTextBox.Text = lowerText;
+            
+            // Display statistics in detailedListBox.
+            detailedListBox.Items.Add("File text is converted to lowercase: " + lowerText);
+            
+            // Find the first and last word alphabetically.
+            // Use a space delimiter to separate all words and create a words array.
+            String[] words = lowerText.Split(' ');
+            
+            // Convert the word array to a wordList.
+            List<String> wordList = words.ToList();
+
+            // Sort the wordList.
+            wordList.Sort();
+
+            // Add items to the detailedListBox.
+            detailedListBox.Items.Add("If sorted alphabetically, " + "'" + wordList.First() + "'" + " is the first word.");
+            detailedListBox.Items.Add("If sorted alphabetically, " + "'" + wordList.Last() + "'" + " is the last word.");
+
+            // Find the longest word.
+            // Declare variables.
+            string longestWord;
+            int currentCount = 0;
+
+            // Use a foreach loop to perform an action for each word in the wordList.
+            foreach (string word in wordList)
+            {
+                // if statement checks if the current word has a higher count than the currentCount. If so,
+                // the word count replaces the currentCount. currentCount is then compared with the next
+                // word count until the longest word is found.
+                if (word.Length > currentCount)
+                {
+                    longestWord = word;
+                    currentCount = word.Length;
+
+                    // Displays the longest word.
+                    detailedListBox.Items.Add("The word " + "'" + longestWord + "'" + " is the longest word in this sentence.");
+                }
+            }
+
+            // Find the word with the most vowels.
+            // Declare variables.
+            int wordVowelCount = 0;
+            int vowelCount = 0;
+            string mostVowels;
+
+            // Use a foreach loop to perform an action for each word in the wordList.
+            foreach (string word in wordList)
+            {
+                // Declared variables
+                int total = 0;
+                // Created a char Hash map for all the vowels and assigned it to a variable.
+                var vowels = new HashSet<char> { 'a', 'e', 'i', 'o', 'u' };
+
+
+                // Used a for loop to iterate through each character of the word.
+                for (int i = 0; i < word.Length; i++)
+                {
+                    // If letter is a vowel, 1 count is added to the total.
+                    if (vowels.Contains(word[i]))
+                    {
+                        wordVowelCount = total++;
+
+                    }
+                    
+                }
+                if (wordVowelCount > vowelCount)
+                {
+                    mostVowels = word;
+                    vowelCount = wordVowelCount;
+
+                    // Display the word with the most vowels.
+                    detailedListBox.Items.Add("'" + mostVowels + "'" + " is the word with the most vowels.");
+                }
+            }
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            richTextBox.Text = " ";
+            detailedListBox.Items.Clear();
+        }
+    }
+}
