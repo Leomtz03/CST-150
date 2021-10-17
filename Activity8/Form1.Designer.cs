65c214062be44f7aeb346411df316fb6e23acf90
Leomtz03 <79104199+Leomtz03@users.noreply.github.com>
2021-10-16 19:16:01 -0700

    First commit

---
+++ Activity8/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿
+namespace Activity8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.stepLabel = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.displayLabel = new System.Windows.Forms.Label();
+            this.carbTextBox = new System.Windows.Forms.TextBox();
+            this.fatTextBox = new System.Windows.Forms.TextBox();
+            this.carbohydratesLabel = new System.Windows.Forms.Label();
+            this.calorieLabel = new System.Windows.Forms.Label();
+            this.panelTitle = new System.Windows.Forms.Panel();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.buttonMax = new System.Windows.Forms.Button();
+            this.buttonMin = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.panelTitle.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // stepLabel
+            // 
+            this.stepLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.stepLabel.AutoSize = true;
+            this.stepLabel.BackColor = System.Drawing.Color.Transparent;
+            this.stepLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stepLabel.ForeColor = System.Drawing.Color.White;
+            this.stepLabel.Location = new System.Drawing.Point(133, 19);
+            this.stepLabel.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.stepLabel.Name = "stepLabel";
+            this.stepLabel.Size = new System.Drawing.Size(253, 24);
+            this.stepLabel.TabIndex = 0;
+            this.stepLabel.Text = "Step 1: Evaluate Your Diet";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("groupBox1.BackgroundImage")));
+            this.groupBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.groupBox1.Controls.Add(this.clearButton);
+            this.groupBox1.Controls.Add(this.calculateButton);
+            this.groupBox1.Controls.Add(this.displayLabel);
+            this.groupBox1.Controls.Add(this.carbTextBox);
+            this.groupBox1.Controls.Add(this.fatTextBox);
+            this.groupBox1.Controls.Add(this.carbohydratesLabel);
+            this.groupBox1.Controls.Add(this.calorieLabel);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(534, 461);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            // 
+            // clearButton
+            // 
+            this.clearButton.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.clearButton.Location = new System.Drawing.Point(137, 340);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(78, 23);
+            this.clearButton.TabIndex = 7;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.calculateButton.Location = new System.Drawing.Point(308, 340);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(78, 23);
+            this.calculateButton.TabIndex = 6;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // displayLabel
+            // 
+            this.displayLabel.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.displayLabel.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.displayLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.displayLabel.Location = new System.Drawing.Point(27, 187);
+            this.displayLabel.Name = "displayLabel";
+            this.displayLabel.Size = new System.Drawing.Size(455, 108);
+            this.displayLabel.TabIndex = 5;
+            // 
+            // carbTextBox
+            // 
+            this.carbTextBox.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.carbTextBox.Location = new System.Drawing.Point(406, 132);
+            this.carbTextBox.Name = "carbTextBox";
+            this.carbTextBox.Size = new System.Drawing.Size(76, 23);
+            this.carbTextBox.TabIndex = 4;
+            // 
+            // fatTextBox
+            // 
+            this.fatTextBox.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.fatTextBox.Location = new System.Drawing.Point(406, 91);
+            this.fatTextBox.Name = "fatTextBox";
+            this.fatTextBox.Size = new System.Drawing.Size(76, 23);
+            this.fatTextBox.TabIndex = 3;
+            // 
+            // carbohydratesLabel
+            // 
+            this.carbohydratesLabel.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.carbohydratesLabel.AutoSize = true;
+            this.carbohydratesLabel.BackColor = System.Drawing.Color.Transparent;
+            this.carbohydratesLabel.Location = new System.Drawing.Point(24, 141);
+            this.carbohydratesLabel.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.carbohydratesLabel.Name = "carbohydratesLabel";
+            this.carbohydratesLabel.Size = new System.Drawing.Size(339, 17);
+            this.carbohydratesLabel.TabIndex = 2;
+            this.carbohydratesLabel.Text = "What is your daily intake of carbohydrates in grams?";
+            // 
+            // calorieLabel
+            // 
+            this.calorieLabel.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.calorieLabel.AutoSize = true;
+            this.calorieLabel.BackColor = System.Drawing.Color.Transparent;
+            this.calorieLabel.Location = new System.Drawing.Point(24, 94);
+            this.calorieLabel.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.calorieLabel.Name = "calorieLabel";
+            this.calorieLabel.Size = new System.Drawing.Size(264, 17);
+            this.calorieLabel.TabIndex = 1;
+            this.calorieLabel.Text = "What is your daily intake of fat in grams?";
+            // 
+            // panelTitle
+            // 
+            this.panelTitle.BackColor = System.Drawing.Color.Maroon;
+            this.panelTitle.Controls.Add(this.buttonMin);
+            this.panelTitle.Controls.Add(this.buttonMax);
+            this.panelTitle.Controls.Add(this.buttonClose);
+            this.panelTitle.Controls.Add(this.stepLabel);
+            this.panelTitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTitle.Location = new System.Drawing.Point(0, 0);
+            this.panelTitle.Name = "panelTitle";
+            this.panelTitle.Size = new System.Drawing.Size(534, 65);
+            this.panelTitle.TabIndex = 2;
+            this.panelTitle.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panelTitle_MouseDown);
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonClose.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonClose.BackgroundImage")));
+            this.buttonClose.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.buttonClose.FlatAppearance.BorderSize = 0;
+            this.buttonClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonClose.ForeColor = System.Drawing.Color.White;
+            this.buttonClose.Location = new System.Drawing.Point(500, -1);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(30, 30);
+            this.buttonClose.TabIndex = 3;
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // buttonMax
+            // 
+            this.buttonMax.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonMax.BackColor = System.Drawing.Color.Maroon;
+            this.buttonMax.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonMax.BackgroundImage")));
+            this.buttonMax.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.buttonMax.FlatAppearance.BorderSize = 0;
+            this.buttonMax.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonMax.ForeColor = System.Drawing.Color.White;
+            this.buttonMax.Location = new System.Drawing.Point(467, -1);
+            this.buttonMax.Name = "buttonMax";
+            this.buttonMax.Size = new System.Drawing.Size(30, 30);
+            this.buttonMax.TabIndex = 4;
+            this.buttonMax.UseVisualStyleBackColor = false;
+            this.buttonMax.Click += new System.EventHandler(this.buttonMax_Click);
+            // 
+            // buttonMin
+            // 
+            this.buttonMin.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonMin.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonMin.BackgroundImage")));
+            this.buttonMin.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.buttonMin.FlatAppearance.BorderSize = 0;
+            this.buttonMin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonMin.ForeColor = System.Drawing.Color.White;
+            this.buttonMin.Location = new System.Drawing.Point(433, -1);
+            this.buttonMin.Name = "buttonMin";
+            this.buttonMin.Size = new System.Drawing.Size(30, 30);
+            this.buttonMin.TabIndex = 5;
+            this.buttonMin.UseVisualStyleBackColor = true;
+            this.buttonMin.Click += new System.EventHandler(this.buttonMin_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.calculateButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(534, 461);
+            this.Controls.Add(this.panelTitle);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(1);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(550, 500);
+            this.Name = "Form1";
+            this.Text = "Evaluation";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panelTitle.ResumeLayout(false);
+            this.panelTitle.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label stepLabel;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label displayLabel;
+        private System.Windows.Forms.TextBox carbTextBox;
+        private System.Windows.Forms.TextBox fatTextBox;
+        private System.Windows.Forms.Label carbohydratesLabel;
+        private System.Windows.Forms.Label calorieLabel;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Panel panelTitle;
+        private System.Windows.Forms.Button buttonMin;
+        private System.Windows.Forms.Button buttonMax;
+        private System.Windows.Forms.Button buttonClose;
+    }
+}
+
---
+++ Activity8/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            // RJ Code Advance
+            // Remove the form title.
+            this.Text = string.Empty;
+            this.ControlBox = false;
+        }
+
+        // RJ Code Advance
+        // Add drag form method.
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
+
+        // Method to calculate calories from fat.
+        private int FatCalories(int fatGrams)
+        {
+            return fatGrams * 9;
+        }
+
+        // Method to calculate calories from carbohydrates.
+        private int CarbCalories(int carbGrams)
+        {
+            return carbGrams * 4;
+        }
+
+        private void Calculate()
+        {
+            // Declared variables.
+            int fatCalories = 0;
+            int carbCalories = 0;
+
+            // Try to convert input to int and assign to fatGrams.
+            if (int.TryParse(fatTextBox.Text, out int fatGrams))
+            {
+                // The fatGrams value is passed when the FatCalories method is called.
+                fatCalories = FatCalories(fatGrams);
+
+                // Try to convert input to int and assign to carbGrams.
+                if (int.TryParse(carbTextBox.Text, out int carbGrams))
+                {
+                    // The carbGrams value is passed when the CarbCalories method is called.
+                    carbCalories = CarbCalories(carbGrams);
+                }
+                else
+                {
+                    // Display error message.
+                    MessageBox.Show("Invalid entry. Enter an integer for carbohydrates.");
+                }
+            }
+            else
+            {
+                // Display error message.
+                MessageBox.Show("Invalid entry. Enter an integer for fat.");
+            }
+
+            // There was input and everything was calculated, there will be a display result.
+            if (fatCalories > 0 && carbCalories > 0)
+            {
+                displayLabel.Text = "Each gram of fat yields 9 calories. Based on the your daily intake of fat in grams, " + 
+                fatCalories + " are calories." + " Each gram of carbohydrates yields 4 calories. " + 
+                "Based on your daily intake of carbohydrate in grams, " + carbCalories + " calories";
+            }
+            else
+            {
+                // Nothing is displayed.
+            }
+            
+        }
+
+        // Method to clear input boxes.
+        private void reset()
+        {
+            fatTextBox.Text = " ";
+            carbTextBox.Text = " ";
+            displayLabel.Text = " ";
+        }
+
+        // Method Calculate() is called.
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            Calculate();
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            reset();
+        }
+
+        // RJ Code Advance
+        // Add the mousedown event
+        private void panelTitle_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        // RJ Code Advance
+        // Add close method.
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        // RJ Code Advance
+        // Add normal and maximize method.
+        private void buttonMax_Click(object sender, EventArgs e)
+        {
+            if(WindowState==FormWindowState.Normal)
+            {
+                this.WindowState = FormWindowState.Maximized;
+            }
+            else
+            {
+                this.WindowState = FormWindowState.Normal;
+            }
+        }
+
+        // RJ Code Advance
+        // Add minimize method.
+        private void buttonMin_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+    }
+}
