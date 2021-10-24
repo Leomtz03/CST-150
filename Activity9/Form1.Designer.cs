eb8602c99a760c5caaa9e8d043d757ca5ac6c6d0
Leomtz03 <79104199+Leomtz03@users.noreply.github.com>
2021-10-23 22:43:43 -0700

    First commit

---
+++ Activity9/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿
+namespace Activity9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.executeBtn = new System.Windows.Forms.Button();
+            this.resultLbl = new System.Windows.Forms.Label();
+            this.num1Lbl = new System.Windows.Forms.Label();
+            this.num2Lbl = new System.Windows.Forms.Label();
+            this.num3Lbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // executeBtn
+            // 
+            this.executeBtn.Location = new System.Drawing.Point(245, 141);
+            this.executeBtn.Margin = new System.Windows.Forms.Padding(7);
+            this.executeBtn.Name = "executeBtn";
+            this.executeBtn.Size = new System.Drawing.Size(175, 51);
+            this.executeBtn.TabIndex = 0;
+            this.executeBtn.Text = "Execute";
+            this.executeBtn.UseVisualStyleBackColor = true;
+            this.executeBtn.Click += new System.EventHandler(this.executeBtn_Click);
+            // 
+            // resultLbl
+            // 
+            this.resultLbl.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.resultLbl.Location = new System.Drawing.Point(294, 489);
+            this.resultLbl.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.resultLbl.Name = "resultLbl";
+            this.resultLbl.Size = new System.Drawing.Size(607, 254);
+            this.resultLbl.TabIndex = 3;
+            // 
+            // num1Lbl
+            // 
+            this.num1Lbl.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.num1Lbl.Location = new System.Drawing.Point(260, 220);
+            this.num1Lbl.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.num1Lbl.Name = "num1Lbl";
+            this.num1Lbl.Size = new System.Drawing.Size(145, 61);
+            this.num1Lbl.TabIndex = 4;
+            // 
+            // num2Lbl
+            // 
+            this.num2Lbl.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.num2Lbl.Location = new System.Drawing.Point(260, 307);
+            this.num2Lbl.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.num2Lbl.Name = "num2Lbl";
+            this.num2Lbl.Size = new System.Drawing.Size(145, 61);
+            this.num2Lbl.TabIndex = 5;
+            // 
+            // num3Lbl
+            // 
+            this.num3Lbl.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.num3Lbl.Location = new System.Drawing.Point(260, 389);
+            this.num3Lbl.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.num3Lbl.Name = "num3Lbl";
+            this.num3Lbl.Size = new System.Drawing.Size(145, 61);
+            this.num3Lbl.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.executeBtn;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(917, 775);
+            this.Controls.Add(this.num3Lbl);
+            this.Controls.Add(this.num2Lbl);
+            this.Controls.Add(this.num1Lbl);
+            this.Controls.Add(this.resultLbl);
+            this.Controls.Add(this.executeBtn);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button executeBtn;
+        private System.Windows.Forms.Label resultLbl;
+        private System.Windows.Forms.Label num1Lbl;
+        private System.Windows.Forms.Label num2Lbl;
+        private System.Windows.Forms.Label num3Lbl;
+    }
+}
+
---
+++ Activity9/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void executeBtn_Click(object sender, EventArgs e)
+        {
+            Activity9 execute = new Activity9();
+
+            resultLbl.Text = execute.Average(2.3, 2.5, 6.2, 7.6, 8.2).ToString();
+        }
+
+        class Activity9
+        {
+            // 1. Write a method that takes two integers and displays their sum.
+            public int Add(int num1, int num2)
+            {
+                return (num1 + num2);
+            }
+
+
+            // 2. Write a method that takes five doubles and returns their average.
+            public double Average(double num1, double num2, double num3, double num4, double num5)
+            {
+                return ((num1 + num2 + num3 + num4 + num5) / 5);
+            }
+
+            // 3. Write a method that returns the sum of two randomly generated integers.
+            public int Sum()
+            {
+                int num1;
+                int num2;
+                int value;
+
+                Random rand = new Random();
+                num1 = rand.Next(0, 10);
+                num2 = rand.Next(0, 10);
+                value = num1 + num2;
+
+                return value;
+            }
+
+            // 4. Write a method that takes three integers and returns true if their sum is
+            // divisible by 3, false otherwise.
+            public Boolean Divisibleby3(int num1, int num2, int num3)
+            {
+                if (num1 % 3 == 0)
+                {
+                    return true;
+                }
+                if (num2 % 3 == 0)
+                {
+                    return true;
+                }
+                if (num3 % 3 == 0)
+                {
+                    return true;
+                }
+                return false;
+            }
+
+            // 5. Write a method that takes two strings and displays the string that has fewer characters.
+            public string compareStrings(string str1, string str2)
+            {
+                if (str1.Length < str2.Length)
+                {
+                    return str2;
+                }
+                else
+                {
+                    return str1;
+                }
+            }
+
+            // 6. Write a method that takes an array of doubles and returns the largest value in the array.
+            public double largestValue()
+            {
+                double[] myArray = { 2.0, 4.0, 7.5, 6.5 };
+
+                double maxValue = myArray.Max();
+
+                return maxValue;
+            }
+
+            // 7. Write a method that generates and returns an array of fifty integer values.
+            public string fiftyIntegers()
+            {
+                Random rand = new Random();
+
+                int[] myArray = new int[50];
+
+                int num = 0;
+
+                for (int i = 0; i < 50; i++)
+                {
+                    // Optional method of generating random numbers.
+                    //myArray[i] = rand.Next(0, 10);
+                    myArray[i] = num + 1;
+                    num++;
+                }
+
+                return string.Join(", ", myArray);
+            }
+
+            // 8. Write a method that takes two bool variables and returns true if they have the same
+            // value, false otherwise.
+            public Boolean StringBoolean(int num1, int num2)
+            {
+                if (num1 == num2)
+                {
+                    return true;
+                }
+                return false;
+            }
+
+            // 9. Write a method that takes an int and a double and returns their product.
+            public double Product(int num1, double num2)
+            {
+                double product = (double)num1 * num2;
+
+                return product;
+            }
+
+            // 10. Write a method that takes a two-dimensional array of integers and returns the
+            // the average of the entries.
+            public decimal TwoDimensionalArray()
+            {
+                const int ROWS = 3;
+                const int COLS = 4;
+                int[,] numbers = { { 1, 2, 3, 4},
+                                {5, 6, 7, 8},
+                                {9, 10 , 11, 12}
+                              };
+
+                decimal sum = 0.0m;
+                decimal average = 0.0m;
+
+                for (int col = 0; col < COLS; col++)
+                {
+                    for (int row = 0; row < ROWS; row++)
+                    {
+                        sum += numbers[row, col];
+                        average = sum / numbers.Length;
+                    }
+
+                }
+                return average;
+            }
+        }
+    }
+}
