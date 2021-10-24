2655842252663ddae986b20a9cf880e9f7c1c561
Leomtz03 <79104199+Leomtz03@users.noreply.github.com>
2021-10-24 16:27:57 -0700

    First commit

---
+++ Activity10/Program.cs
@@ -0,0 +1,47 @@
+﻿//Corrected by <Leonides Martinez>
+
+//use for IC08
+//Lydi's code
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //make some sets
+            Set A = new Set();
+            Set B = new Set();
+            Set C = new Set();
+
+            //put some stuff in the sets
+            Random r = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                A.addElement(r.Next(4));
+                B.addElement(r.Next(12));
+            }
+
+            //display each set and the union
+            Console.WriteLine("A: " + A);
+            Console.WriteLine("B: " + B);
+            // Corrected
+            // Transferred Set A to Set C to later reference the original list for Set A.
+            C.union(A);
+            Console.WriteLine("A union B: " + A.union(B));
+            
+            //display original sets (should be unchanged)
+            Console.WriteLine("After union operation");
+            //Corrected
+            // Displayed Set C which was Set A before union.
+            Console.WriteLine("A: " + C);
+            Console.WriteLine("B: " + B);
+
+        }
+    }
+}
---
+++ Activity10/Set.cs
@@ -0,0 +1,89 @@
+﻿//Corrected by <Leonides Martinez>
+
+//use for IC08
+//Lydi's code
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Activity10
+{
+    class Set
+    {
+        private List<int> elements;
+
+
+        public Set()
+        {
+            elements = new List<int>();
+        }
+
+        public bool addElement(int val)
+        {
+            if (containsElement(val)) return false;
+            else
+            {
+                elements.Add(val);
+                return true;
+            }
+        }
+
+        private bool containsElement(int val)
+        {
+            // Corrected
+            // Must Flag variable to indicate whether the it contains element.
+            bool containsElement = false;
+
+            for (int i = 0; i < elements.Count; i++)
+            {
+                
+                if (val == elements[i])
+                {
+                    // Corrected
+                    // return is causing the for loop to only loop once. Causing i++ to
+                    // be unreachable.
+                    //return true;
+                    containsElement = true;
+                }
+                else
+                {
+                    // Corrected
+                    //return false;
+                    // Instead, else statement could be deleted or left blank.
+                }
+                    
+            }
+            return containsElement;
+        }
+
+        public override string ToString()
+        {
+            string str = "";
+            foreach (int i in elements)
+            {
+                str += i + " ";
+            }
+            return str;
+        }
+
+        public void clearSet()
+        {
+            elements.Clear();
+        }
+
+        public Set union(Set rhs)
+        {
+            for (int i = 0; i < rhs.elements.Count; i++)
+            {
+                this.addElement(rhs.elements[i]);
+
+            }
+            // Corrected
+            // return is displaying Set passed into method instead of new list.
+            //return rhs;
+            return this;
+        }
+    }
+}
