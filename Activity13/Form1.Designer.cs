08c4208eb001d344bc6339bfe4f3730343cd934a
Leomtz03 <79104199+Leomtz03@users.noreply.github.com>
2021-10-30 10:00:10 -0700

    First commit

---
+++ Activity13/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿
+namespace Activity13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.newGameBtn = new System.Windows.Forms.Button();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.lbl4 = new System.Windows.Forms.Label();
+            this.lbl7 = new System.Windows.Forms.Label();
+            this.lbl5 = new System.Windows.Forms.Label();
+            this.lbl6 = new System.Windows.Forms.Label();
+            this.lbl8 = new System.Windows.Forms.Label();
+            this.lbl9 = new System.Windows.Forms.Label();
+            this.winnerLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // newGameBtn
+            // 
+            this.newGameBtn.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.newGameBtn.Location = new System.Drawing.Point(124, 278);
+            this.newGameBtn.Name = "newGameBtn";
+            this.newGameBtn.Size = new System.Drawing.Size(75, 23);
+            this.newGameBtn.TabIndex = 1;
+            this.newGameBtn.Text = "New Game";
+            this.newGameBtn.UseVisualStyleBackColor = false;
+            this.newGameBtn.Click += new System.EventHandler(this.newGameBtn_Click);
+            // 
+            // lbl1
+            // 
+            this.lbl1.BackColor = System.Drawing.Color.White;
+            this.lbl1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbl1.Font = new System.Drawing.Font("Times New Roman", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.Location = new System.Drawing.Point(17, 14);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(88, 65);
+            this.lbl1.TabIndex = 3;
+            this.lbl1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl2
+            // 
+            this.lbl2.BackColor = System.Drawing.Color.White;
+            this.lbl2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbl2.Font = new System.Drawing.Font("Times New Roman", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl2.Location = new System.Drawing.Point(111, 14);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(88, 65);
+            this.lbl2.TabIndex = 4;
+            this.lbl2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl3
+            // 
+            this.lbl3.BackColor = System.Drawing.Color.White;
+            this.lbl3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbl3.Font = new System.Drawing.Font("Times New Roman", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl3.Location = new System.Drawing.Point(205, 14);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(88, 65);
+            this.lbl3.TabIndex = 5;
+            this.lbl3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl4
+            // 
+            this.lbl4.BackColor = System.Drawing.Color.White;
+            this.lbl4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbl4.Font = new System.Drawing.Font("Times New Roman", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl4.Location = new System.Drawing.Point(17, 86);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(88, 65);
+            this.lbl4.TabIndex = 6;
+            this.lbl4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl7
+            // 
+            this.lbl7.BackColor = System.Drawing.Color.White;
+            this.lbl7.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbl7.Font = new System.Drawing.Font("Times New Roman", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl7.Location = new System.Drawing.Point(17, 158);
+            this.lbl7.Name = "lbl7";
+            this.lbl7.Size = new System.Drawing.Size(88, 65);
+            this.lbl7.TabIndex = 7;
+            this.lbl7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl5
+            // 
+            this.lbl5.BackColor = System.Drawing.Color.White;
+            this.lbl5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbl5.Font = new System.Drawing.Font("Times New Roman", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl5.Location = new System.Drawing.Point(111, 86);
+            this.lbl5.Name = "lbl5";
+            this.lbl5.Size = new System.Drawing.Size(88, 65);
+            this.lbl5.TabIndex = 8;
+            this.lbl5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl6
+            // 
+            this.lbl6.BackColor = System.Drawing.Color.White;
+            this.lbl6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbl6.Font = new System.Drawing.Font("Times New Roman", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl6.Location = new System.Drawing.Point(205, 86);
+            this.lbl6.Name = "lbl6";
+            this.lbl6.Size = new System.Drawing.Size(88, 65);
+            this.lbl6.TabIndex = 9;
+            this.lbl6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl8
+            // 
+            this.lbl8.BackColor = System.Drawing.Color.White;
+            this.lbl8.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbl8.Font = new System.Drawing.Font("Times New Roman", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl8.Location = new System.Drawing.Point(111, 158);
+            this.lbl8.Name = "lbl8";
+            this.lbl8.Size = new System.Drawing.Size(88, 65);
+            this.lbl8.TabIndex = 10;
+            this.lbl8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl9
+            // 
+            this.lbl9.BackColor = System.Drawing.Color.White;
+            this.lbl9.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbl9.Font = new System.Drawing.Font("Times New Roman", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl9.Location = new System.Drawing.Point(205, 158);
+            this.lbl9.Name = "lbl9";
+            this.lbl9.Size = new System.Drawing.Size(88, 65);
+            this.lbl9.TabIndex = 11;
+            this.lbl9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // winnerLbl
+            // 
+            this.winnerLbl.BackColor = System.Drawing.Color.White;
+            this.winnerLbl.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.winnerLbl.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.winnerLbl.Location = new System.Drawing.Point(17, 240);
+            this.winnerLbl.Name = "winnerLbl";
+            this.winnerLbl.Size = new System.Drawing.Size(276, 23);
+            this.winnerLbl.TabIndex = 12;
+            this.winnerLbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SlateGray;
+            this.ClientSize = new System.Drawing.Size(309, 328);
+            this.Controls.Add(this.winnerLbl);
+            this.Controls.Add(this.lbl9);
+            this.Controls.Add(this.lbl8);
+            this.Controls.Add(this.lbl6);
+            this.Controls.Add(this.lbl5);
+            this.Controls.Add(this.lbl7);
+            this.Controls.Add(this.lbl4);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.newGameBtn);
+            this.Name = "Form1";
+            this.Text = "Tic-Tac-Toe";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button newGameBtn;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Label lbl4;
+        private System.Windows.Forms.Label lbl7;
+        private System.Windows.Forms.Label lbl5;
+        private System.Windows.Forms.Label lbl6;
+        private System.Windows.Forms.Label lbl8;
+        private System.Windows.Forms.Label lbl9;
+        private System.Windows.Forms.Label winnerLbl;
+    }
+}
+
---
+++ Activity13/Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Declared variables. All methods can use variables.
+        string zero = 0.ToString();
+        string letterX = "X";
+        string letterO = "O";
+
+        // Created list for controls.
+        // Meant for DisplayPlayerName method.
+        List<Control> controls = new List<Control>();
+
+        public void TicTacToe()
+        {
+            // page 497
+            // Created instance of random number.
+            Random rand = new Random();
+
+            // Declared constant variables.
+            const int ROWS = 3;
+            const int COLS = 3;
+
+            // Declared two dimensional array.
+            string[,] doubleArray = new string[ROWS, COLS];
+
+            // Added controls to controls list.
+            // Meant for DisplayPlayerName method.
+            controls.Add(lbl1);
+            controls.Add(lbl2);
+            controls.Add(lbl3);
+            controls.Add(lbl4);
+            controls.Add(lbl5);
+            controls.Add(lbl6);
+            controls.Add(lbl7);
+            controls.Add(lbl8);
+            controls.Add(lbl9);
+
+            // Used a for loop to iterate through each element.
+            for (int row = 0; row < ROWS; row++)
+            {
+                for (int col = 0; col < COLS; col++)
+                {
+                    // Assigned a random number to each element.
+                    doubleArray[row, col] = rand.Next(2).ToString();
+
+                    // If statement converts a number zero to letterO.
+                    if (doubleArray[row, col] == zero)
+                    {
+                        doubleArray[row, col] = letterO;
+                    }
+                    // Otherwise, converts to letterX.
+                    else
+                    {
+                        doubleArray[row, col] = letterX;
+                    }
+
+                    // Each control label is assigned an element and its value.
+                    lbl1.Text = doubleArray[0, 0];
+                    lbl2.Text = doubleArray[0, 1];
+                    lbl3.Text = doubleArray[0, 2];
+                    lbl4.Text = doubleArray[1, 0];
+                    lbl5.Text = doubleArray[1, 1];
+                    lbl6.Text = doubleArray[1, 2];
+                    lbl7.Text = doubleArray[2, 0];
+                    lbl8.Text = doubleArray[2, 1];
+                    lbl9.Text = doubleArray[2, 2];
+
+                }
+            }
+        }
+
+        // Method checks for winner.
+        // Each if statement checks for every possible way of winning in Tic-Tac-Toe.
+        public string CheckForWinner()
+        {
+            // For example, if statement checks whether the top line has the same symbol.
+            if (lbl1.Text == lbl2.Text && lbl2.Text == lbl3.Text)
+            {
+                if (lbl1.Text == letterO)
+                {
+                    winnerLbl.Text = "Player Y is winner!";
+                }
+                else
+                {
+                    winnerLbl.Text = "Player X is winner!";
+                }
+                //winnerLbl.Text = "Player " + lbl1.Text.ToString() + " is winner!";
+            }
+            else if (lbl4.Text == lbl5.Text && lbl5.Text == lbl6.Text)
+            {
+                if (lbl4.Text == letterO)
+                {
+                    winnerLbl.Text = "Player Y is winner!";
+                }
+                else
+                {
+                    winnerLbl.Text = "Player X is winner!";
+                }
+            }
+            else if (lbl7.Text == lbl8.Text && lbl8.Text == lbl9.Text)
+            {
+                if (lbl7.Text == letterO)
+                {
+                    winnerLbl.Text = "Player Y is winner!";
+                }
+                else
+                {
+                    winnerLbl.Text = "Player X is winner!";
+                }
+            }
+            else if (lbl1.Text == lbl5.Text && lbl5.Text == lbl9.Text)
+            {
+                if (lbl1.Text == letterO)
+                {
+                    winnerLbl.Text = "Player Y is winner!";
+                }
+                else
+                {
+                    winnerLbl.Text = "Player X is winner!";
+                }
+            }
+            else if (lbl3.Text == lbl5.Text && lbl5.Text == lbl7.Text)
+            {
+                if (lbl3.Text == letterO)
+                {
+                    winnerLbl.Text = "Player Y is winner!";
+                }
+                else
+                {
+                    winnerLbl.Text = "Player X is winner!";
+                }
+            }
+            else if (lbl1.Text == lbl4.Text && lbl4.Text == lbl7.Text)
+            {
+                if (lbl1.Text == letterO)
+                {
+                    winnerLbl.Text = "Player Y is winner!";
+                }
+                else
+                {
+                    winnerLbl.Text = "Player X is winner!";
+                }
+            }
+            else if (lbl2.Text == lbl5.Text && lbl5.Text == lbl8.Text)
+            {
+                if (lbl2.Text == letterO)
+                {
+                    winnerLbl.Text = "Player Y is winner!";
+                }
+                else
+                {
+                    winnerLbl.Text = "Player X is winner!";
+                }
+            }
+            else if (lbl3.Text == lbl6.Text && lbl6.Text == lbl9.Text)
+            {
+                if (lbl3.Text == letterO)
+                {
+                    winnerLbl.Text = "Player Y is winner!";
+                }
+                else
+                {
+                    winnerLbl.Text = "Player X is winner!";
+                }
+            }
+            else
+            {
+                winnerLbl.Text = "Game is a Draw!";
+            }
+            return null;
+        }
+
+        // Method is under construction still.
+        // Intended to create a method that would convert and display a letterO to "Y".
+        // There by replacing the second set of if else statements in CheckForWinner().
+        /*public string DisplayPlayerName()
+        {
+            string temp;
+            string a;
+            string b;
+
+            /*for (int i = 0; i < controls.Count; i++)
+            {
+                if (controls[i].Text == letterO)
+                {
+                    winnerLbl.Text = "Player Y is winner!";
+                }
+                else
+                {
+                    winnerLbl.Text = "Player X is winner!";
+                }
+            }
+            return null;
+        }*/
+
+        private void newGameBtn_Click(object sender, EventArgs e)
+        {
+            // On click event, called the methods below.
+            TicTacToe();
+            CheckForWinner();
+        }
+    }
+}
