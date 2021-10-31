b7d08569df34871d027049a0a3967d18f2795f8b
Leomtz03 <79104199+Leomtz03@users.noreply.github.com>
2021-10-31 11:52:43 -0700

    First commit

---
+++ Activity14/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿
+namespace Activity14
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.myImageList = new System.Windows.Forms.ImageList(this.components);
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.statementLbl = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dateCheckBox = new System.Windows.Forms.CheckBox();
+            this.nameCheckBox = new System.Windows.Forms.CheckBox();
+            this.displayBtn = new System.Windows.Forms.Button();
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.nameLbl = new System.Windows.Forms.Label();
+            this.dateLbl = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // myImageList
+            // 
+            this.myImageList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("myImageList.ImageStream")));
+            this.myImageList.TransparentColor = System.Drawing.Color.Transparent;
+            this.myImageList.Images.SetKeyName(0, "Landscape.jpg");
+            this.myImageList.Images.SetKeyName(1, "MonaLisa.jpg");
+            this.myImageList.Images.SetKeyName(2, "Museum.jpg");
+            this.myImageList.Images.SetKeyName(3, "Beach.jpg");
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox.Location = new System.Drawing.Point(313, 22);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(249, 260);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.Location = new System.Drawing.Point(31, 50);
+            this.listBox.Margin = new System.Windows.Forms.Padding(1);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(165, 134);
+            this.listBox.TabIndex = 1;
+            // 
+            // statementLbl
+            // 
+            this.statementLbl.AutoSize = true;
+            this.statementLbl.Location = new System.Drawing.Point(28, 22);
+            this.statementLbl.Name = "statementLbl";
+            this.statementLbl.Size = new System.Drawing.Size(115, 13);
+            this.statementLbl.TabIndex = 2;
+            this.statementLbl.Text = "Select a type of image:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(31, 188);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(165, 72);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Select Window Backcolor";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 42);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(87, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "LightSkyBlue";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 19);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(94, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "LightSlateGray";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dateCheckBox);
+            this.groupBox2.Controls.Add(this.nameCheckBox);
+            this.groupBox2.Location = new System.Drawing.Point(31, 266);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(165, 68);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Select Image Details";
+            // 
+            // dateCheckBox
+            // 
+            this.dateCheckBox.AutoSize = true;
+            this.dateCheckBox.Location = new System.Drawing.Point(6, 39);
+            this.dateCheckBox.Name = "dateCheckBox";
+            this.dateCheckBox.Size = new System.Drawing.Size(49, 17);
+            this.dateCheckBox.TabIndex = 1;
+            this.dateCheckBox.Text = "Date";
+            this.dateCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // nameCheckBox
+            // 
+            this.nameCheckBox.AutoSize = true;
+            this.nameCheckBox.Location = new System.Drawing.Point(6, 16);
+            this.nameCheckBox.Name = "nameCheckBox";
+            this.nameCheckBox.Size = new System.Drawing.Size(54, 17);
+            this.nameCheckBox.TabIndex = 0;
+            this.nameCheckBox.Text = "Name";
+            this.nameCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // displayBtn
+            // 
+            this.displayBtn.BackColor = System.Drawing.Color.Blue;
+            this.displayBtn.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.displayBtn.ForeColor = System.Drawing.Color.White;
+            this.displayBtn.Location = new System.Drawing.Point(347, 369);
+            this.displayBtn.Name = "displayBtn";
+            this.displayBtn.Size = new System.Drawing.Size(75, 23);
+            this.displayBtn.TabIndex = 5;
+            this.displayBtn.Text = "Display";
+            this.displayBtn.UseVisualStyleBackColor = false;
+            this.displayBtn.Click += new System.EventHandler(this.displayBtn_Click);
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.BackColor = System.Drawing.Color.Blue;
+            this.clearBtn.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.clearBtn.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clearBtn.ForeColor = System.Drawing.Color.White;
+            this.clearBtn.Location = new System.Drawing.Point(470, 369);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(75, 23);
+            this.clearBtn.TabIndex = 6;
+            this.clearBtn.Text = "Clear";
+            this.clearBtn.UseVisualStyleBackColor = false;
+            this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
+            // 
+            // nameLbl
+            // 
+            this.nameLbl.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameLbl.Location = new System.Drawing.Point(396, 299);
+            this.nameLbl.Name = "nameLbl";
+            this.nameLbl.Size = new System.Drawing.Size(100, 23);
+            this.nameLbl.TabIndex = 9;
+            this.nameLbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // dateLbl
+            // 
+            this.dateLbl.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateLbl.Location = new System.Drawing.Point(396, 322);
+            this.dateLbl.Name = "dateLbl";
+            this.dateLbl.Size = new System.Drawing.Size(100, 23);
+            this.dateLbl.TabIndex = 10;
+            this.dateLbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.displayBtn;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.CancelButton = this.clearBtn;
+            this.ClientSize = new System.Drawing.Size(601, 404);
+            this.Controls.Add(this.dateLbl);
+            this.Controls.Add(this.nameLbl);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.displayBtn);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.statementLbl);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.pictureBox);
+            this.Margin = new System.Windows.Forms.Padding(1);
+            this.Name = "Form1";
+            this.Text = "Activity 14";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList myImageList;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.Label statementLbl;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox dateCheckBox;
+        private System.Windows.Forms.CheckBox nameCheckBox;
+        private System.Windows.Forms.Button displayBtn;
+        private System.Windows.Forms.Button clearBtn;
+        private System.Windows.Forms.Label nameLbl;
+        private System.Windows.Forms.Label dateLbl;
+    }
+}
+
---
+++ Activity14/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity14
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listBox.Items.Add("Landscape");
+            listBox.Items.Add("MonaLisa");
+            listBox.Items.Add("Museum");
+            listBox.Items.Add("Beach");
+        }
+
+        private void displayBtn_Click(object sender, EventArgs e)
+        {
+            int index = listBox.SelectedIndex;
+
+            DateTime currentDateTime = DateTime.Now;
+            
+            if (listBox.SelectedIndex == -1)
+            {
+                MessageBox.Show("Please select an image type.");
+            }
+            else
+            {
+                pictureBox.Image = myImageList.Images[index];
+
+                if (radioButton1.Checked)
+                {
+                    this.BackColor = Color.LightSlateGray;
+                }
+
+                if (radioButton2.Checked)
+                {
+                    this.BackColor = Color.LightSkyBlue;
+                }
+                if (nameCheckBox.Checked)
+                {
+                    nameLbl.Text = listBox.SelectedItem.ToString();
+                }
+
+                if (dateCheckBox.Checked)
+                {
+                    dateLbl.Text = currentDateTime.ToString("d");
+                }
+            }
+        }
+
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            pictureBox.Image = null;
+            radioButton1.Checked = false;
+            radioButton2.Checked = false;
+            nameCheckBox.Checked = false;
+            dateCheckBox.Checked = false;
+            nameLbl.Text = "";
+            dateLbl.Text = "";
+        }
+    }
+}
